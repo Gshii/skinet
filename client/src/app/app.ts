79f95a97bae71987d350f471b0396dc2a109b24e
Gshii <117040064+Gshii@users.noreply.github.com>
2026-03-09 18:12:08 +0200

    client added

---
+++ client/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatSlideToggleModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  title = 'SkiNet';
+}
