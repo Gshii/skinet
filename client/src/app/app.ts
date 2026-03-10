bd7cc67737983e2bac1a0b34dfb1539d5f196dc2
Gshii <117040064+Gshii@users.noreply.github.com>
2026-03-10 21:04:22 +0200

    component header

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,13 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
+  baseUrl = 'https://localhost:5091/api/';
+  private http = inject(HttpClient);
   title = 'SkiNet';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response) => {
+        console.log(response);
+        this.products = response.data;
+      },
+      error: (error) => console.log(error),
+      complete: () => console.log('Request completed'),
+    });
+  }
 }
